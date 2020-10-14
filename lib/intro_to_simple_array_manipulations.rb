8165268d86e68a2a4528ef0bc3280398fdb723d7
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-14 19:21:39 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,13 @@ def pop_with_args(array)
   second_removed = array.pop
   return [second_removed, first_removed]
 end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array)
+  first_shifted = array.shift
+  second_shifted = array.shift
+  return [first_shifted, second_shifted]
+end
